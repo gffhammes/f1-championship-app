51ea56424ef88b031a6b771a5dd28cc4f6aed506
Guilherme Hammes <gffhammes@gmail.com>
2022-09-13 13:48:43 -0300

    fix: add conditional chain to dates

--- src/components/ComingUp/GpCard.tsx
+++ src/components/ComingUp/GpCard.tsx
@@ -1,48 +1,64 @@
-import { Box, Stack, Typography } from '@mui/material';
-import Image from 'next/image';
-import React from 'react'
-import { getFormattedWeekendDates } from '../../helpers/getFormattedDate';
+import { Box, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import { getFormattedWeekendDates } from "../../helpers/getFormattedDate";
 
 type Props = {
   gpData: any;
-}
+};
 
 const sxCardWrapper = {
-  backgroundColor: '#E9E9E9',
+  backgroundColor: "#E9E9E9",
   borderRadius: 2,
-  p: 2
-}
+  p: 2,
+};
 
 const sxCountryFlag = {
-  position: 'relative',
-  height: '2rem',
-  aspectRatio: '1 / 1',
+  position: "relative",
+  height: "2rem",
+  aspectRatio: "1 / 1",
   borderRadius: 1,
-  border: '1px solid #bcbcbc'
-}
+  border: "1px solid #bcbcbc",
+};
 
 export const GpCard = ({ gpData }: Props) => {
   const formattedDate = getFormattedWeekendDates(gpData);
 
+  console.log(gpData.Circuit.Location.country);
+
   return (
     <Box sx={sxCardWrapper}>
-      <Stack direction='row' alignItems='flex-start' spacing={2} >
-        
+      <Stack direction="row" alignItems="flex-start" spacing={2}>
         <Box sx={sxCountryFlag}>
           <Image
             src={`https://countryflagsapi.com/png/${gpData.Circuit.Location.country}`}
-            alt=''
-            layout='fill'
-            objectFit='cover'
-            style={{ borderRadius: '4px' }}
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            style={{ borderRadius: "4px" }}
           />
         </Box>
         <Box>
-          <Typography fontSize={18} fontWeight={600} lineHeight={1}>{gpData.raceName}</Typography>
-          <Typography fontSize={12} lineHeight={1} sx={{ mt: .5, color: 'rgba(54, 54, 54, 0.8)' }}>{gpData.Circuit.circuitName}</Typography>
-          <Typography fontSize={12} fontWeight={600} lineHeight={1} sx={{ mt: 1.25, color: 'rgba(54, 54, 54, 0.8)' }}>{formattedDate}</Typography>
+          <Typography fontSize={18} fontWeight={600} lineHeight={1}>
+            {gpData.raceName}
+          </Typography>
+          <Typography
+            fontSize={12}
+            lineHeight={1}
+            sx={{ mt: 0.5, color: "rgba(54, 54, 54, 0.8)" }}
+          >
+            {gpData.Circuit.circuitName}
+          </Typography>
+          <Typography
+            fontSize={12}
+            fontWeight={600}
+            lineHeight={1}
+            sx={{ mt: 1.25, color: "rgba(54, 54, 54, 0.8)" }}
+          >
+            {formattedDate}
+          </Typography>
         </Box>
       </Stack>
     </Box>
-  )
-}
+  );
+};
--- src/helpers/getFormattedDate.ts
+++ src/helpers/getFormattedDate.ts
@@ -1,49 +1,64 @@
-export const getFormattedDate = (date: string, time: string) => {  
+export const getFormattedDate = (date: string, time: string) => {
   const dateString = `${date}T${time}`;
   const localeDate = new Date(dateString);
 
   const formatNumber = (number: number) => {
-    return number.toLocaleString('pt-BR', {minimumIntegerDigits: 2, useGrouping:false})
-  }
+    return number.toLocaleString("pt-BR", {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    });
+  };
 
-  const formattedDate = `${formatNumber(localeDate.getDate())}/${formatNumber(localeDate.getMonth() + 1)}`
-  const formattedTime = `${formatNumber(localeDate.getHours())}:${formatNumber(localeDate.getMinutes())}`
+  const formattedDate = `${formatNumber(localeDate.getDate())}/${formatNumber(
+    localeDate.getMonth() + 1
+  )}`;
+  const formattedTime = `${formatNumber(localeDate.getHours())}:${formatNumber(
+    localeDate.getMinutes()
+  )}`;
 
   return {
     formattedDate,
     formattedTime,
-  }
-}
-
+  };
+};
 
 export const getFormattedWeekendDates = (gpData: any) => {
-  const { formattedDate: fpOneDate } = getFormattedDate(gpData.FirstPractice.date, gpData.FirstPractice.time);
-  const { formattedDate: fpTwoDate } = getFormattedDate(gpData.SecondPractice.date, gpData.SecondPractice.time);
-  const { formattedDate: fpThreeDate } = getFormattedDate(gpData.ThirdPractice.date, gpData.ThirdPractice.time);
-  const { formattedDate: qualifyingDate } = getFormattedDate(gpData.Qualifying.date, gpData.Qualifying.time);
-  const { formattedDate: raceDate } = getFormattedDate(gpData.date, gpData.time);
+  const { formattedDate: fpOneDate } = getFormattedDate(
+    gpData.FirstPractice?.date,
+    gpData.FirstPractice?.time
+  );
+  const { formattedDate: fpTwoDate } = getFormattedDate(
+    gpData.SecondPractice?.date,
+    gpData.SecondPractice?.time
+  );
+  const { formattedDate: fpThreeDate } = getFormattedDate(
+    gpData.ThirdPractice?.date,
+    gpData.ThirdPractice?.time
+  );
+  const { formattedDate: qualifyingDate } = getFormattedDate(
+    gpData.Qualifying?.date,
+    gpData.Qualifying?.time
+  );
+  const { formattedDate: raceDate } = getFormattedDate(
+    gpData?.date,
+    gpData?.time
+  );
 
-  const dates = [
-    fpOneDate,
-    fpTwoDate,
-    fpThreeDate,
-    qualifyingDate,
-    raceDate,
-  ]
+  const dates = [fpOneDate, fpTwoDate, fpThreeDate, qualifyingDate, raceDate];
 
   const uniqueDates = dates.filter((item, pos) => {
     return dates.indexOf(item) == pos;
-  })
+  });
 
   let formattedDateString = "";
 
   uniqueDates.forEach((date, index) => {
     if (index === 0) {
-      formattedDateString = date
+      formattedDateString = date;
       return;
     }
-    formattedDateString = formattedDateString + " • " + date 
-  })
+    formattedDateString = formattedDateString + " • " + date;
+  });
 
-  return formattedDateString
-}
+  return formattedDateString;
+};
